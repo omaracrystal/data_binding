6d692e874f63ccef89c502c52270a558576c5888
Crystal <crystal@Zachs-MacBook-Air.local>
2015-10-26 15:30:44 -0600

    main2.js solved the data binding in vanilla javascript

--- main.js
+++ main.js
@@ -4,3 +4,35 @@ var scope = {};
 var classNamesArray = ['name'];
 
 // ** functions ** //
+
+//iterate through classNamesArray
+  // grab DOM elements associated with each class
+  // return an array of DOM elements
+
+function getElements() {
+  var outputArray = [];
+  for (var i = 0; i < classNamesArray.length; i++) {
+    outputArray.push(document.getElementsByClassName(
+      classNamesArray[i][0]));
+  }
+  return outputArray;
+}
+
+// iterate through each dom element
+// attach onkeyup event to each
+// perform *some* sort of action to bind the two inputs
+
+function domBinding(elements) {
+  for (var index in elements) {
+    elements[index].onkeyup = function () {
+      for var(index in elements) {
+        elements[index].value = this.value;
+      }
+    };
+  };
+}
+
+
+// ** function calls ** //
+
+var domElement = getElements();
---
+++ main2.js
@@ -0,0 +1,25 @@
+var $scope = {};
+(function () {
+    var bindClasses = ["name"];
+    var attachEvent = function (classNames) {
+        classNames.forEach(function (className) {
+            var elements = document.getElementsByClassName(className);
+            for (var index in elements) {
+                elements[index].onkeyup = function () {
+                    for (var index in elements) {
+                        elements[index].value = this.value;
+                    }
+                };
+            }
+            Object.defineProperty($scope, className, {
+                set: function (newValue) {
+                    for (var index in elements) {
+                        elements[index].value = newValue;
+                    }
+                }
+            });
+
+        });
+    };
+    attachEvent(bindClasses);
+})();
